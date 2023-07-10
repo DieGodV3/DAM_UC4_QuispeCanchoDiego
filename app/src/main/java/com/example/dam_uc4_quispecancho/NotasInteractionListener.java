787409a3e5eb92f6e685c14b44f701176579753c
diego quispe <diego.qc2013@gmail.com>
2023-07-10 14:42:11 -0500

    Notas en Columnas

---
+++ app/src/main/java/com/example/dam_uc4_quispecancho/NotasInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.dam_uc4_quispecancho;
+
+public interface NotasInteractionListener {
+
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+
+}
