22f3253be071c12f6092ac9f0cbf80c58ac97463
diego quispe <diego.qc2013@gmail.com>
2023-07-10 14:20:51 -0500

    Login

---
+++ app/src/main/java/com/example/dam_uc4_quispecancho/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dam_uc4_quispecancho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,NotasActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
